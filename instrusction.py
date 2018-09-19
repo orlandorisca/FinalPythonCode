b9f6381cf11f6ab670a6b04344c873ba5eab3f94
Student <student@2024-01-203-24.aii.stu>
2018-09-18 18:28:14 -0700

    updates

---
+++ instrusction.py
@@ -0,0 +1,5 @@
+create many cubes at on random but it also can change with diferent model, also can create a animation if you want,
+
+Copy the code into the maya pytho script node
+and you will run
+it will automatically run with cubes
